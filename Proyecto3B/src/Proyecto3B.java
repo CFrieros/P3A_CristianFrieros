03029f955b5ff40630d55961b3dd219abd450e1b
CFrieros <151049301+CFrieros@users.noreply.github.com>
2023-11-30 16:45:49 +0100

    Update Proyecto3B.java

--- Proyecto3B/src/Proyecto3B.java
+++ Proyecto3B/src/Proyecto3B.java
@@ -1,6 +1,6 @@
 
 public class Proyecto3B {
 	public static void main(String[] args) {
-		System.out.println("Cristian Frieros Recio Proyecto3B");
+		System.out.println("Cristian Frieros Recio Proyecto3B\nEstoy llegando al final");
 	}
 }
